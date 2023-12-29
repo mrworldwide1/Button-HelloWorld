d4d797adfb85453b40fdadb567d913d2ac6424a1
Lucas Leung <noemail>
2023-12-29 16:37:45 -0500

    basic setup

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
 # simple incremental game to test out pygame
 # may add more unique click sounds in the future
 
-import pygame, sys
+import pygame
+import sys
 
 # Setup
-
 pygame.init()
 
 screenWidth = 1000
 screenHeight = 500
 maxFPS = 60
-clock = pygame.time.Clock()
 screen = pygame.display.set_mode((screenWidth, screenHeight))
 pygame.display.set_caption('Clicker')
 
@@ -20,9 +19,9 @@ def main():
     # music
     ost = pygame.mixer.music
     ost.load('xDeviruchi - Title Theme .wav')
-    ost.play(-1) # loop parameter set to true
+    ost.play(-1) # loop set to true
 
-    # endless loop, every gameplay action happens here
+    # entire game runs inside this endless loop, keeps the code running forever
     while True:
         for event in pygame.event.get():
             # break loop when player quits
@@ -31,6 +30,7 @@ def main():
                 sys.exit()
     
         pygame.display.update()
-        clock.tick(maxFPS)
+        pygame.time.Clock().tick(maxFPS)
+
 
 main()
