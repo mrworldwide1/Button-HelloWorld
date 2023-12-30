5d07433e14be1b78611dd707b2dbae2726c90ad2
Lucas Leung <noemail>
2023-12-29 19:25:55 -0500

    k

--- main.py
+++ main.py
@@ -3,34 +3,33 @@ import sys
 
 pygame.init()
 
+# setup game window
 screenWidth = 1000
 screenHeight = 500
+pygame.display.set_caption('Clicker')
+screen = pygame.display.set_mode((screenWidth, screenHeight))
+maxFPS = 60
+
+# define variables
 buttonWidth = 200
 buttonHeight = 200
-maxFPS = 60
 ostName = "xDeviruchi - Title Theme .wav"
 bgColour = "linen"
 buttonName = "button.png"
-pygame.display.set_caption('Clicker')
-display = pygame.display.set_mode((screenWidth, screenHeight))
-
 
 # surfaces are like layers in photoshop! you fill each layer with stuff like text/color/images
-# then put the layers on the canvas
-# here, layers are called Surfaces
-# creates instance of Surface and fills it
+# then put the layers on the canvas. here, layers are called Surfaces
 bg = pygame.Surface((screenWidth, screenHeight))
 bg.fill(bgColour)
 
 buttonSurface = pygame.image.load(buttonName)
-
+buttonSurface = pygame.transform.scale(buttonSurface, (200,200))
 
 # Core part that actually runs everything
 def main():
-
     ost = pygame.mixer.music
     ost.load(ostName)
-    ost.play(-1) # loops
+    ost.play()
 
     # entire game runs inside this loop, keeps the code running forever
     while True:
@@ -40,9 +39,9 @@ def main():
                 pygame.quit()
                 sys.exit()
 
-        # place bg surface onto display surface, at this point
-        display.blit(bg, (0,0))
-        display.blit(button, (0,0))
+        # place surfaces onto screen surface, at this point
+        screen.blit(bg, (0,0))
+        screen.blit(buttonSurface, (0,0))
 
         pygame.display.update()
         pygame.time.Clock().tick(maxFPS)
